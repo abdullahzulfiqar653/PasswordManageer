2731022df22251b19cb28b596706757dcb03a38a
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-08-03 16:13:02 +0500

    auth urls added for neuro rsa

--- api/rsa_urls.py
+++ api/rsa_urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenVerifyView
+
 from api.views import (
-    RecipientListCreateView,
-    KeyPairListCreateView,
+    UserSignInView,
+    UserSignUpView,
     EncryptMessageView,
     DecryptMessageView,
+    KeyPairListCreateView,
+    RecipientListCreateView,
 )
-
 
 urlpatterns = [
     # =====================================================
@@ -33,5 +36,15 @@ urlpatterns = [
         "recipients/decrypt-message",
         DecryptMessageView.as_view(),
         name="recipients-decrypt-message",
+    ),
+    # =====================================================
+    # User
+    # =====================================================
+    path("user/token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("user/generate-token/", UserSignInView.as_view(), name="generate-token"),
+    path(
+        "user/generate-pass-phrase/",
+        UserSignUpView.as_view(),
+        name="generate-pass-phrase",
     ),
 ]
