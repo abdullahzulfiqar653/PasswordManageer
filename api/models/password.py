adda967ffd2712b939eb6b5fe0a90b8ce644bb6a
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-08-27 08:14:54 +0500

    Password CreateList Serializer updated

--- api/models/password.py
+++ api/models/password.py
@@ -15,7 +15,7 @@ class Password(BaseModel):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         # Unique constraint across title, username, and url for each user
-        unique_together = ['title', 'username', 'url', 'user']
+        unique_together = ["title", "username", "url", "user"]
--- api/serializers/password.py
+++ api/serializers/password.py
@@ -4,13 +4,24 @@ from rest_framework import exceptions
 
 
 class PasswordCreateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Password
-        fields = "__all__"
+        fields = ["id", "title", "username", "password", "url", "notes", "emoji"]
+        # read_only_fields = ["user"]
 
-    def validate_user(self, user):
-        if user.id != self.context["request"].user.id:
-            raise exceptions.PermissionDenied(
-                "Permission denied: User ID in payload does not match request user ID."
+    def validate(self, attrs):
+        user = self.context["request"].user
+        if user.passwords.filter(
+            title=attrs["title"],
+            username=attrs["username"],
+            url=attrs.get("url"),
+        ).exists():
+            raise serializers.ValidationError(
+                {"error": "An entry with this title, username, and URL already exists."}
             )
-        return user
+        return attrs
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
