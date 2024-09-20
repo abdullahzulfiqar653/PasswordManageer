69669459f4272e4bbb93787f2b1b1bb698369059
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-09-20 12:53:14 +0500

    neuromail URLS for signin signup added

--- NeuroMail/urls.py
+++ NeuroMail/urls.py
@@ -1,1 +1,20 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import TokenVerifyView
+
+from main.views import (
+    UserSignInView,
+    UserSignUpView,
+)
+
+urlpatterns = [
+    # =====================================================
+    # User
+    # =====================================================
+    path("user/token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("user/generate-token/", UserSignInView.as_view(), name="generate-token"),
+    path(
+        "user/generate-pass-phrase/",
+        UserSignUpView.as_view(),
+        name="generate-pass-phrase",
+    ),
+]
