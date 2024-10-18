93618d2b4f614ff388a69bec3a7dc0c79c1cbb6a
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-10-18 19:50:40 +0500

    password manager folder filter

--- PasswordManager/views/password/password.py
+++ PasswordManager/views/password/password.py
@@ -1,11 +1,13 @@
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from PasswordManager.serializers.password import PasswordSerializer
 
 
 class PasswordListCreateView(generics.ListCreateAPIView):
     serializer_class = PasswordSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ["title", "username", "url", "notes", "emoji"]
+    filterset_fields = ["folder"]
 
     def get_queryset(self):
         return self.request.user.passwords.all()
