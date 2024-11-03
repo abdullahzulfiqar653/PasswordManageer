7364f9f5768ecfa03278ba9871d94dc0a42a6483
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-11-03 18:59:59 +0500

    signup updated!

--- main/serializers/signup.py
+++ main/serializers/signup.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from main.utils import generate_passphrase, hash_passphrase
@@ -12,7 +13,21 @@ class UserSignUpSerializer(serializers.Serializer):
         while User.objects.filter(username=passphrase).exists():
             passphrase = generate_passphrase()
             hashed_passphrase = hash_passphrase(passphrase)
-        user = User.objects.create(username=passphrase)
-        user.set_password(hashed_passphrase)
-        user.save()
+        url = "https://apiresonance.neuronus.net/api/user/register"
+        headers = {"accept": "application/json", "Content-Type": "application/json"}
+        data = {"seed": passphrase}
+
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            response.raise_for_status()
+            response_data = response.json()
+            if response_data.get("success") == True:
+                user = User.objects.create(username=passphrase)
+                user.set_password(hashed_passphrase)
+                user.save()
+        except Exception as e:
+            print(f"Error: {e}")
+            raise serializers.ValidationError(
+                {"error": "Failed to create passphrase please refresh page"}
+            )
         return {"pass_phrase": passphrase}
