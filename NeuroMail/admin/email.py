c8f0593bc8314128c06321dc0c7bccb78357f6c2
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-01-02 02:27:56 +0500

    admin updated

--- NeuroMail/admin/email.py
+++ NeuroMail/admin/email.py
@@ -61,21 +61,10 @@ class EmailAdmin(admin.ModelAdmin):
     )
 
     readonly_fields = ("total_size",)
-    actions = ["mark_as_seen", "mark_as_starred"]
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         return form
-
-    def mark_as_seen(self, request, queryset):
-        queryset.update(is_seen=True)
-
-    mark_as_seen.short_description = "Mark selected emails as seen"
-
-    def mark_as_starred(self, request, queryset):
-        queryset.update(is_starred=True)
-
-    mark_as_starred.short_description = "Mark selected emails as starred"
 
     inlines = [EmailAttachmentInline, EmailRecipientInline]
 
