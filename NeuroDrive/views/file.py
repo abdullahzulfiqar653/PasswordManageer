f81d2fa621cc17e3853f85746f74a99c3b724b46
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-01-24 23:16:20 +0500

    file updated views

--- NeuroDrive/views/file.py
+++ NeuroDrive/views/file.py
@@ -1,30 +1,13 @@
-from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
+from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
+
+from main.services.s3 import S3Service
+from NeuroDrive.models.file import File
+from NeuroDrive.models.shared_access import SharedAccess
 
 from NeuroDrive.serializers.file import FileSerializer
-from NeuroDrive.permissions import (
-    IsFileOwner,
-    IsDirectoryOwner,
-    IsOwnerOrSharedDirectory,
-)
-
-
-class FileListCreateView(generics.ListCreateAPIView):
-    serializer_class = FileSerializer
-    search_fields = ["name"]
-    filterset_fields = ["is_starred"]
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-
-    def get_permissions(self):
-        """
-        Instantiates and returns the list of permissions that this view requires.
-        """
-        if self.request.method == "POST":
-            return [IsDirectoryOwner()]
-        return [IsOwnerOrSharedDirectory()]
-
-    def get_queryset(self):
-        return self.request.directory.files.all().order_by("created_at")
+from NeuroDrive.permissions import IsFileOwner, IsDirectoryOwner
 
 
 class FileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -35,8 +18,53 @@ class FileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         instance.owner.remove_size(instance.size)
         return super().perform_destroy(instance)
 
+    def get_object(self):
+        """
+        Retrieve the file object, and check if the user is either the owner or has access through SharedAccess.
+        """
+        # Retrieve the file object based on the primary key
+        obj = super().get_object()
+
+        # Verify if the user is the owner
+        if obj.owner != self.request.user:
+            # Check if the user has shared access to the file with READ or FULL permission
+            shared_access = SharedAccess.objects.filter(
+                user=self.request.user,
+                item=obj,
+                permission_type__in=[
+                    SharedAccess.Permission.READ,
+                    SharedAccess.Permission.FULL,
+                ],
+            )
+
+            if not shared_access.exists():
+                raise PermissionDenied(
+                    "You do not have permission to access this file."
+                )
+        s3_client = S3Service()
+        obj.url = s3_client.generate_presigned_url(obj.s3_url)
+        return obj
+
     def get_queryset(self):
-        return self.request.directory.files.all().order_by("created_at")
+        """
+        Return all files that the user owns or has access to through SharedAccess.
+        """
+        user = self.request.user
+
+        # Using Q objects to combine both conditions in a single queryset
+        files = File.objects.filter(
+            Q(owner=user)  # User owns the file
+            | Q(
+                shared_accesses__user=user,
+                shared_accesses__permission_type__in=[
+                    SharedAccess.Permission.READ,
+                    SharedAccess.Permission.WRITE,
+                    SharedAccess.Permission.FULL,
+                ],
+            )  # User has shared access to the file
+        ).distinct()  # Ensure distinct results
+        
+        return files
 
 
 class FileDirecoryUpdateView(generics.UpdateAPIView):
